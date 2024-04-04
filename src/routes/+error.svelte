cbdfcd4a899fbd79c09bfdd5f7bfce93e0c799ed
BombayV <OMGmsebastian@gmail.co>
2024-04-04 00:35:50 -0400

    finally added favicon

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,7 @@
-<div class="bg-neutral-950 text-white w-full h-screen flex flex-col items-center justify-center font-raleway">
-  <h1 class="text-9xl font-semibold">404</h1>
-  <p class="text-3xl font-light mt-4">Page not found</p>
-  <a class="btn w-fit mt-4" href="/">Go back to home</a>
+<div
+	class="bg-neutral-950 text-white w-full h-screen flex flex-col items-center justify-center font-raleway"
+>
+	<h1 class="text-9xl font-semibold">404</h1>
+	<p class="text-3xl font-light mt-4">Page not found</p>
+	<a class="btn w-fit mt-4" href="/">Go back to home</a>
 </div>
